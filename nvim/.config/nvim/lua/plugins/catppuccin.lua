79fb0d2b223e1a18ed267a0f2034ba544d06fb74
yakovlievv <yakovlievv25@gmail.com>
2025-03-23 11:36:29 +0200

    nvim-experimental stuff

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -5,7 +5,6 @@ return {
     priority = 1000,
     config = function()
         require("catppuccin").setup({
-            transparent_background = true,
             integrations = {
                 neotree = true,
                 treesitter = true,
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {'lua', 'javascript'},
+            ensure_installed = {'lua', 'javascript', 'python',},
             highlight = {enabled = true},
             indent = { enable = true },
         })
