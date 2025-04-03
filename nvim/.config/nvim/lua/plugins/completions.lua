9df28c7a0347744628702056bd823d2da83a78d8
yakovlievv <yakovlievv25@gmail.com>
2025-04-03 18:59:07 +0300

    nvim change

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -2,23 +2,50 @@ return {
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
-            'neovim/nvim-lspconfig',
+            "onsails/lspkind.nvim",
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-path',
-            'L3MON4D3/LuaSnip',
+            { 'L3MON4D3/LuaSnip', build = "make install_jsregexp" },
             'saadparwaiz1/cmp_luasnip',
             'rafamadriz/friendly-snippets',
         },
         config = function()
             local cmp = require'cmp'
+            local luasnip = require('luasnip')
+            local lspkind = require('lspkind')
             require("luasnip.loaders.from_vscode").lazy_load()
+            cmp.setup {
+                formatting = {
+                    format = lspkind.cmp_format({
+                        maxwidth = {
+                            -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                            -- can also be a function to dynamically calculate max width such as
+                            -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+                            menu = 50, -- leading text (labelDetails)
+                            abbr = 50, -- actual suggestion item
+                        },
+                        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
-            cmp.setup({
+                        -- The function below will be called before any actual modifications from lspkind
+                        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                        before = function (entry, vim_item)
+                            -- ...
+                            return vim_item
+                        end
+                    })
+                },
+                sources = {
+                    { name = 'luasnip' }, -- For luasnip users.
+                    { name = 'nvim_lsp' },
+                    { name = 'path' },
+                    { name = 'buffer' },
+                },
                 snippet = {
                     expand = function(args)
-                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        luasnip.lsp_expand(args.body) -- For `luasnip` users.
                     end,
                 },
                 window = {
@@ -26,20 +53,34 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-CR>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true, behaviour = cmp.ConfirmBehavior.Insert,}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' }, -- For luasnip users.
-                }, {
-                        { name = 'buffer' },
-                        { name = 'path' },
-                    })
-            })
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-y>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-j>"] = cmp.mapping.select_next_item(), -- Move down in menu
+                    ["<C-k>"] = cmp.mapping.select_prev_item(), -- Move up in menu
+                    ["<C-l>"] = cmp.mapping(function(fallback)
+                        if luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }), -- Expand or jump forward
+
+                    ["<C-h>"] = cmp.mapping(function(fallback)
+                        if luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }), -- Jump backward
+                })
+            }
+            luasnip.config.set_config {
+                history = true
+
+            }
         end
     }
 }
