31a4dfef06b90a3ff1e86a2795877d113613ea52
yakovlievv <yakovlievv25@gmail.com>
2025-04-30 18:29:08 +0300

    big update

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -225,7 +225,7 @@ return {
             mappings = {
               ["<bs>"] = "navigate_up",
               ["."] = "set_root",
-              ["<C-h>"] = "toggle_hidden",
+              ["<C-i>"] = "toggle_hidden",
               ["/"] = "fuzzy_finder",
               ["D"] = "fuzzy_finder_directory",
               ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
