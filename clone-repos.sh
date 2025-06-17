e40d0c0c305f1925641d6302fac6c6ac1b4d4afd
yakovlievv <yakovlievv25@gmail.com>
2025-06-17 17:37:05 +0300

    Removed submodule zshrc/.config/zsh/zsh-autosuggestions

---
+++ clone-repos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/zsh-autosuggestions/
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting/
+git clone --depth=1 https://github.com/tmux-plugins/tpm.git ~/.config/tmux/tpm/
