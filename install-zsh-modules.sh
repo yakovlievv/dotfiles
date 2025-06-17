bb299847f34430df328b41df8ec3512d7e820f52
yakovlievv <yakovlievv25@gmail.com>
2025-06-17 17:37:05 +0300

    Removed submodule zshrc/.config/zsh/zsh-autosuggestions

--- install-zsh-modules.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/zsh-autosuggestions/
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting/
