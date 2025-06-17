bb3544d9c15ec8572b739b5073b272c77259200f
yakovlievv <yakovlievv25@gmail.com>
2025-06-17 17:04:53 +0300

    a lot of fucking around later

---
+++ install-zsh-modules.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/zsh-autosuggestions/
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting/
