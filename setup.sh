8cf3995c02e11258c090441d99427620e6ad2674
yakovlievv <yakovlievv25@gmail.com>
2026-01-28 17:50:12 +0200

    submodules

--- setup.sh
+++ setup.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-export DOTFILES = "$HOME/dots"
-
-if [[ "$OSTYPE" == darwin* ]]; then
-    rm -rf "$HOME/Documents/bin"
-    ln -sfnv "$DOTFILES/bin" "$HOME/Documents/"
-    export PATH="$PATH:$HOME/Documents/bin"
-
-elif [[ "$OSTYPE" == linux-* ]]; then
-    rm -rf "$HOME/bin"
-    ln -sfnv "$DOTFILES/bin" "$HOME/bin"
-    export PATH="$PATH:$HOME/bin"
-fi
-
+rm -rf "$HOME/bin"
+ln -sfnv "dotfiles/bin" "$HOME/bin"
+export PATH="$PATH:$HOME/bin"
