a0bd12d78a02ffed6b945ed13212d98a64501b7b
yakovlievv <yakovlievv25@gmail.com>
2026-02-16 14:45:27 +0200

    emacs big update

---
+++ doom/custom.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/org/archive/20260127165333-productivity_setup.org_archive"
+     "/Users/yako/org/archive/tasks.org_archive"
+     "/Users/yako/org/archive/20260127161003-dzereltse_website.org_archive"
+     "/Users/yako/org/roam/20260127165333-productivity_setup.org"
+     "/Users/yako/org/roam/20260127161003-dzereltse_website.org"
+     "/Users/yako/org/habits.org" "/Users/yako/org/tasks.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
