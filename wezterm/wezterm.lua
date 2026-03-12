9b97d14295ad8b5e15ab7120fea0db45270846ed
yakovlievv <yakovlievv25@gmail.com>
2026-03-12 19:50:03 +0200

    tried to fix something but it's all lost and hopeless

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -38,6 +38,7 @@ config.cursor_blink_rate = 500
 config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 
 config.enable_tab_bar = false
+config.enable_kitty_keyboard = true
 
 -- macOS option as alt
 config.send_composed_key_when_left_alt_is_pressed = false
